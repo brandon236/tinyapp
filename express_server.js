c6366cddfd5a2c998a7059707dbe1c46cab5dd61
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-06-09 05:13:15 +0000

    Added redirects

--- express_server.js
+++ express_server.js
@@ -49,13 +49,19 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const short = generateRandomString();
+  urlDatabase[short] = req.body['longURL'];
+  res.redirect(`/urls/${short}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 
